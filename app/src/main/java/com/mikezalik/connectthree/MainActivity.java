28c9538320f4a41c1ea27b9749661b13c896dfc6
mikezalik <mike.zalik@gmail.com>
2020-01-22 12:41:41 -0800

    Adds state tracking for grid and winning positions to determine winner.

--- app/src/main/java/com/mikezalik/connectthree/MainActivity.java
+++ app/src/main/java/com/mikezalik/connectthree/MainActivity.java
@@ -8,12 +8,22 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //0: yellow, 1: red
+    //0: yellow, 1: red, 2:empty
+
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
 
     int activePlayer = 0;
 
     public void dropIn (View view)  {
         ImageView counter = (ImageView) view;
+
+        counter.getTag();
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        gameState[tappedCounter] = activePlayer;
 
         counter.setTranslationY(-1500);
 
@@ -28,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+        
     }
 
     @Override
